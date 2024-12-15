e8a5229faadb66278f935d4f7bb9d7f22a1ef4be
Alan Bang <alan.bang@outlook.com>
2024-12-15 22:09:19 +0800

    site config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,8 +44,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/abangfundi/abangfundi/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
