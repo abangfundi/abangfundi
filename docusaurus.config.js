2fc98ba0e33c28df4c8dd13eb3ac61d824db32f7
Alan Bang <alan.bang@outlook.com>
2025-01-07 00:12:38 +0800

    blog: 2025

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,10 +41,9 @@ const config = {
       ({
         docs: false,
         blog: {
+          blogSidebarTitle: 'All our posts',
+          blogSidebarCount: 'ALL',
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/abangfundi/abangfundi/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
